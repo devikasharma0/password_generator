ffe9ea775728053bb09dd0d39483b4f090f40e83
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-08-21 16:25:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project_veet
+ This is a random password generator
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/hero' 
+
+function App() {
+  return <div>
+
+    <Hero/>
+  </div>
+}
+
+export default App //If we export the app component as default, we can import it with any name.
